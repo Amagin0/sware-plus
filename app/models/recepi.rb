8d663c7fdca6f17bd13205ea014486a141471ffe
Amagin0 <amaginyos.ask.given.u@gmail.com>
2022-01-07 15:47:43 +0000

    [add]レシピカード文字数設定

--- app/models/recepi.rb
+++ app/models/recepi.rb
@@ -1,4 +1,7 @@
 class Recepi < ApplicationRecord
   attachment :recepi_image
   belongs_to :customer
+
+  validates :recepi_title, length: { maximum: 32 }
+  validates :recepi_describe, length: { maximum: 32 }
 end
