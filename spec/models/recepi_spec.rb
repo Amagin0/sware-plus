c4600fa88acdb2d7a10cc8ee6f13b6b6ee1504e7
Amagin0 <amaginyos.ask.given.u@gmail.com>
2022-01-25 23:21:16 +0900

    [save]

--- spec/models/recepi_spec.rb
+++ spec/models/recepi_spec.rb
@@ -6,11 +6,18 @@ require 'rails_helper'
     describe '正常系の機能' do
       context '作成する' do
         it "正しく登録できること" do
-          customer = Customer.create(name: "tanaka", email: "hoge@gma.cc", password: "hogehoge", password_confirmation: "hogehoge")
-          params = {"recepi_title"=>"aaa",recepi_image_id: "111",  "recepi_ingredients_attributes"=>{"0"=>{"ingredient"=>"aaa", "_destroy"=>"false"}}}
+          customer = Customer.create(name: "tanaka", email: "hoge@gmail.com", password: "hogehoge", password_confirmation: "hogehoge")
+          params = {"recepi_title"=>"aaa","recepi_image_id"=>"111",
+                    "recepi_ingredients_attributes"=>{"0"=>{"ingredient"=>"aaa", "_destroy"=>"false"}},
+                    # "how_to_makes_attributes"=>{"0"=>{"recepi_make"=>"bbb","how_to_image"=>"{}", "_destroy"=>"false"}},
+                    # "tags_attributes"=>{"1"=>{"recepi_id"=>"1", "genre_id"=>"1", "_destroy"=>"false"}}
+
+          }
           recepi = Recepi.new(params)
           recepi.customer_id = customer.id
           expect{recepi.save}.to change{recepi.recepi_ingredients.count}.from(0).to(1)
+          # expect{recepi.save}.to change{recepi.how_to_makes.count}.from(0).to(1)
+          # and change{recepi.tags.count}.from(0).to(1)
         end
       end
     end
