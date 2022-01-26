3b821e4461eec79a566188f7a36f906313f03768
Amagin0 <amaginyos.ask.given.u@gmail.com>
2022-01-26 23:17:44 +0900

    [add,fix]READMEを編集、退会ページ追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,16 @@ Rails.application.routes.draw do
   end
 
   resources :customers, only: %i[new index show edit update] do
-    get 'my_recepi', on: :member
-    get 'my_favorite', on: :member
+    member do
+      get 'my_recepi'
+      get 'my_favorite'
+    end
+
+    collection do
+      get 'unsubscribe'
+      patch 'deleted'
+    end
+
     resource :relationships, only: %i[create destroy]
     get 'followings' => 'relationships#followings', as: 'followings'
     get 'followers' => 'relationships#followers', as: 'followers'
